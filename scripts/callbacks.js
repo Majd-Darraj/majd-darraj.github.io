8d08060b965acabcb180ecd9465c73ad497a593b
Majd <majd.darraj@hotmail.com>
2018-04-27 12:49:34 +0200

    Create AJAX function | fetchData(url, callbackFn)

--- scripts/callbacks.js
+++ scripts/callbacks.js
@@ -1,8 +1,23 @@
 /*-- 
 Get button element and assign it to 'btn' variable
 --*/
-let btn = document.querySelector('.btn');
+const btn = document.querySelector('.btn');
 btn.addEventListener('click', () => {
-    console.log("Clicked");
+    console.log("Clicked")
 });
 
+
+/*--
+Ajax function - Fetch data and parse the text to JSON format
+--*/
+function fetchData(url, callbackFn) {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', () => {
+        console.log("Data loaded.");
+        // Parse plain text to JSON and assign it to var data
+        const data = JSON.parse(xhr.responseText);
+        callbackFn(data);
+    });
+    xhr.open('GET', url);
+    xhr.send();
+}
